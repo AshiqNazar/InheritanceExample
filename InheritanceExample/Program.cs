483fca97dd9f7b33f523d0785d051d1ee2202c96
AshiqNazar90 <khanashiq18@gmail.com>
2022-05-05 09:53:19 +0530

    Add project files.

---
+++ InheritanceExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car car = new Car();
+
+            //Console.WriteLine(car.speed);
+            //Console.WriteLine(car.wheel);
+            //car.go();
+
+            //Bike bike = new Bike();
+            //Console.WriteLine(bike.wheel);
+            //Console.WriteLine(bike.speed);
+            //bike.go();
+
+            try
+            {
+                int[] a = { 3, 4, 5 };
+                Console.WriteLine(a[3]);
+
+
+
+            }
+            catch (Exception ex)
+            {
+                // logic
+             
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("This is should work any way");
+            }
+
+           
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InheritanceExample/Vechile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+    public class Vechile
+    {
+
+        //Parentclass
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("The vechicle is moving");
+        }
+
+
+    }
+}
